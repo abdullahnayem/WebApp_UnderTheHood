3c5f46c84acaba072cf1f92a5585ab40b2ecc281
Abdullah Md.Nayem <67652220+abdullahnayem@users.noreply.github.com>
2021-12-20 18:17:19 +0600

    Update Login.cshtml.cs

--- WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
+++ WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,6 +15,23 @@ namespace WebApp_UnderTheHood.Pages.Account
         public Credential Credential { get; set; }
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            if (!ModelState.IsValid) return;
+
+            //verify the credential
+            if (Credential.UserName== "admin" && Credential.Password=="123")
+            {
+                // creating the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@admin.com"),
+
+                };
+            }
         }
     }
     public class Credential
